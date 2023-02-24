c69bd66928670d0532bb4b5ed38f12daa5f3a89f
phjsicist <68902369+phjsicist@users.noreply.github.com>
2023-02-24 10:53:40 +0100

    Overengineer Image class

--- src/img2ascii/image.py
+++ src/img2ascii/image.py
@@ -7,56 +7,124 @@ import numpy as np
 from typing import Optional
 
 
+IMAGE_MODES = {'RGBA': 4,
+               'RGB': 3,
+               'GRAY': 1}
+
+IMAGE_FORMATS = {'float32': 1,
+                 'uint8': 255}
+
+
 class Image:
-    def __init__(self, img: np.ndarray) -> None:
-        self.image = img
+    def __init__(self, img: np.ndarray, mode: str='RGBA') -> None:
+        height = img.shape[0]
+        width = img.shape[1]
+        h_axis = np.linspace(0, 1, height)
+        w_axis = np.linspace(0, 1, width)
+
+        if str(img.dtype) in IMAGE_FORMATS.keys():
+            new_img = img/IMAGE_FORMATS[str(img.dtype)]
+        else:
+            raise TypeError(f'Unexpected image format passed to Image: {img.dtype}')
+
+        if img.ndim == 2:
+            self._contentR = interp2d(w_axis, h_axis, new_img)
+            self._contentG = self._contentR
+            self._contentB = self._contentR
+            self._contentA = lambda w, h: 0
+
+        elif img.ndim == 3 and img.shape[2] == 3:
+            self._contentR = interp2d(w_axis, h_axis, new_img[:, :, 0])
+            self._contentG = interp2d(w_axis, h_axis, new_img[:, :, 1])
+            self._contentB = interp2d(w_axis, h_axis, new_img[:, :, 2])
+            self._contentA = lambda w, h: 0
+
+        elif img.ndim == 3 and img.shape[2] == 4:
+            self._contentR = interp2d(w_axis, h_axis, new_img[:, :, 0])
+            self._contentG = interp2d(w_axis, h_axis, new_img[:, :, 1])
+            self._contentB = interp2d(w_axis, h_axis, new_img[:, :, 2])
+            self._contentA = interp2d(w_axis, h_axis, new_img[:, :, 3])
+
+        else:
+            raise TypeError(f'ndarray passed to Image is of unexpected shape: {img.shape}, \
+                            should be (*, *), (*, *, 3) or (*, *, 4).')
+
+        self.mode = mode
+        self.shape = img.shape[:2]
 
     @property
-    def image(self) -> np.ndarray:
-        return self._image
+    def mode(self) -> str:
+        return self._mode
     
-    @image.setter
-    def image(self, img) -> None:
-        if len(img.shape) == 2:
-            self._image = img
+    @mode.setter
+    def mode(self, mode: str) -> None:
+        if not mode in IMAGE_MODES.keys():
+            raise ValueError(f'Image received unexpected value for mode: {mode}, \
+                             should be in {IMAGE_MODES.keys()}')
+        
+        self._mode = mode
+        self._number_of_channels = IMAGE_MODES[mode]
 
-        elif len(img.shape) == 3:
-            self._image = np.empty(img.shape[:2])
-            self._image[:, :] = (img[:, :, 0] + img[:, :, 1] + img[:, :, 2]) / 3    
+    @property
+    def shape(self) -> tuple:
+        return self._shape
+    
+    @shape.setter
+    def shape(self, shape: int | tuple) -> None:
+        if type(shape) == int:
+            height = shape
+            width = int(self.shape[1] * height/self.shape[0])
+        else:
+            height = shape[0]
+            width = shape[1]
 
-        self.shape = self._image.shape
-        self.height = self.shape[0]
-        self.width = self.shape[1]
+        self._shape = (height, width)
 
     @classmethod
     def from_path(cls, path: str) -> Image:
         return cls(img=plt.imread(path))
     
-    def resize(self, height: int, width: Optional[int]=None) -> Image:
-        height_factor = height / self.height
+    def as_array(self) -> np.ndarray:
+        height = self.shape[0]
+        width = self.shape[1]
+        h_axis = np.linspace(0, 1, height)
+        w_axis = np.linspace(0, 1, width)
 
-        if width == None:
-            width = int(self.width * height_factor)
-        width_factor = width / self.width
-        
-        old_w_coord = np.arange(0, self.width)*width_factor
-        old_h_coord = np.arange(0, self.height)*height_factor
-        
-        f = interp2d(old_w_coord, old_h_coord, self.image)
-        
-        new_w_coord = np.arange(0, width)
-        new_h_coord = np.arange(0, height)
-        new_image = f(new_w_coord, new_h_coord)
+        if self.mode == 'RGBA':
+            arr = np.empty((*self.shape, 4))
+            arr[:, :, 0] = self._contentR(w_axis, h_axis)
+            arr[:, :, 1] = self._contentG(w_axis, h_axis)
+            arr[:, :, 2] = self._contentB(w_axis, h_axis)
+            arr[:, :, 3] = self._contentA(w_axis, h_axis)
 
-        return self.__class__(new_image)
+        if self.mode == 'RGB':
+            arr = np.empty((*self.shape, 3))
+            arr[:, :, 0] = self._contentR(w_axis, h_axis)
+            arr[:, :, 1] = self._contentG(w_axis, h_axis)
+            arr[:, :, 2] = self._contentB(w_axis, h_axis)
 
-    def transpose(self) -> Image:
-        return self.__class__(np.flip(self.image))
+        if self.mode == 'GRAY':
+            arr = np.zeros(self.shape)
+            arr += self._contentR(w_axis, h_axis)
+            arr += self._contentG(w_axis, h_axis)
+            arr += self._contentB(w_axis, h_axis)
+            arr /= 3
+
+        return arr
+
+    def flip(self) -> None:
+        self._contentR = lambda w, h: self._contentR(-w, -h)
+        self._contentG = lambda w, h: self._contentG(-w, -h)
+        self._contentB = lambda w, h: self._contentB(-w, -h)
+        self._contentA = lambda w, h: self._contentA(-w, -h)
     
     def show(self, dpi: int=60) -> None:
-        plt.figure(figsize=(self.width/dpi, self.height/dpi))
+        plt.figure(figsize=(self.shape[0]/dpi, self.shape[1]/dpi))
         plt.axis('off')
-        plt.imshow(self.image, cmap='Greys')
+        if self.mode == 'GRAY':
+            plt.imshow(self.as_array(), cmap='Greys')
+        else:
+            plt.imshow(self.as_array())
         plt.show()
 
 
